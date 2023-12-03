f2bea46b25f6a5c3b1f2764854da89b8836436fa
Rasika Doranepola <rasikanuwan1998@gmail.com>
2023-12-03 20:30:37 +0530

    Test7

--- Main.sh
+++ Main.sh
@@ -9,7 +9,7 @@ pipeline {
                 script{
                     def directory = pwd()
                     echo "${directory}"
-                    ls
+                    sh "ls"
                 }
             }
         }
