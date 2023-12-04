029c2b60ceb1f16c44af15d0d9c190a1a4b055b6
Rasika Doranepola <rasikanuwan1998@gmail.com>
2023-12-04 08:30:30 +0530

    Test22

--- Main.sh
+++ Main.sh
@@ -17,7 +17,7 @@ pipeline {
         stage('Execute the script'){
             steps{
                 script{
-                    bat '"C:\Program Files\Git\bin\bash.exe -c" helloworld.sh'
+                    bat '"C:\\Program Files\\Git\\bin\\bash.exe -c" helloworld.sh'
                 }
             }
         }
